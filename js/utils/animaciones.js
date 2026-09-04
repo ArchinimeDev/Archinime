58782d6123bb2a1736420d0ff7d996054e72a3c1
ArchinimeDev <archinime77@gmail.com>
2026-09-04 16:26:58 -0500

    Add files via upload

--- js/utils/animaciones.js
+++ js/utils/animaciones.js
@@ -4,11 +4,9 @@
 */
 
 const videoList = [
-  { id: 'hola', src: 'assets/videos/hola.mp4', preset: { threshold: 0.25, diff: 0, soft: 100 } },
   { id: 'rem',  src: 'assets/videos/rem.mp4',  preset: { threshold: 0.10, diff: 0, soft: 100  } },
   { id: 'rimuru',  src: 'assets/videos/rimuru.mp4',  preset: { threshold: 0.10, diff: 0, soft: 100  } },
   { id: 'reze',  src: 'assets/videos/reze.mp4',  preset: { threshold: 0.10, diff: 0, soft: 100  } },
   { id: 'levi',  src: 'assets/videos/levi.mp4',  preset: { threshold: 0.10, diff: 0, soft: 100  } },
   { id: 'gojo',  src: 'assets/videos/gojo.mp4',  preset: { threshold: 0.10, diff: 0, soft: 100  } },
-  { id: 'shinobu',  src: 'assets/videos/shinobu.mp4',  preset: { threshold: 0.10, diff: 0, soft: 100  } },
 ];
